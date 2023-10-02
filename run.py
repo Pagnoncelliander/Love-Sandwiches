e9b4bebbd06e412effecc82aca338a7fe6bf752f
Pagnoncelliander <>
2023-10-02 20:04:07 +0000

    adjust validation to check

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import gspread
-
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,33 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
-    get sales figures input the user
+    Get sales figures input from the user.
     """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-    print("please enter sales data from the last market")
-    print("Data should be six numbers, separated by comas.")
-    print("example: 10,20,30,40,50,60\n")
-
-    data_str =  input("Enter your data here: ")
-   
+    data_str = input("Enter your data here: ")
     sales_data = data_str.split(",")
-    print(sales_data)
-    
+    validate_data(sales_data)
+
+
 def validate_data(values):
     """
-    inside the try, converts all string values into integers
-    Raises ValueError if string cannot be converted into int
-    or if there arent exactly 6 values
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-        )
+            )
     except ValueError as e:
-        print(f"invalidate data {e}, please try again.\n")
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
-
